ee177f0a358428296cbf7e58541bf17514b24371
Xiaozhe Yao <askxzyao@gmail.com>
2023-10-06 14:18:26 +0200

    update token count

--- fmengine/utils/token_count.py
+++ fmengine/utils/token_count.py
@@ -2,14 +2,15 @@ import os
 from multiprocessing import Pool
 from transformers import AutoTokenizer
 
-
-def count(tokenizer, text):
-    return len(tokenizer.encode(text, add_special_tokens=False))
-
+def count_tokens(args):
+    return len(args[0].encode(args[1]))
 
 def parallel_count(tokenizer_name, texts):
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-    # get cpu count
     n_processor = os.cpu_count()
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    
     with Pool(n_processor) as pool:
-        return sum(pool.map(count, [tokenizer] * len(texts), texts))
+        return sum(pool.map(count_tokens, [(tokenizer, text) for text in texts]))
+
+def get_epoch_size(batch_size, seq_len, dp_degree, total_tokens):
+    return int(total_tokens / (batch_size * seq_len * dp_degree))
